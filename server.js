6c2db4cc52977a58cbd742844e840ad8417f37ad
Jan Keromnes <janx@linux.com>
2011-11-22 09:33:40 +0100

    Bye bye Makefile
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var net = require('net');
 
 var server = net.Server ( function(socket) {
-  console.log('socket says yo!');
+  console.log('incoming socket!');
   socket.on('data', function(data) {
-    console.log('socket says',data);
+    console.log('socket says '+data);
   });
   socket.write('hi, socket!');
 }).listen(0x5Ad);
+
+console.log('server ready!');
 
